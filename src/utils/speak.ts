a42cfee18142fd171b242699727eb05c3109d5a6
NOOMBO <elack.official.1@gmail.com>
2026-03-07 18:41:57 -0500

    Add speak function for speech synthesis

---
+++ src/utils/speak.ts
@@ -0,0 +1,7 @@
+// AVA responds via browser speech synthesis
+export function speak(text: string) {
+  const utterance = new SpeechSynthesisUtterance(text);
+  utterance.rate = 1;
+  utterance.pitch = 1;
+  speechSynthesis.speak(utterance);
+}
