4aafad25252f2a8bc61cf921fe635b52e2751655
NOOMBO <elack.official.1@gmail.com>
2026-03-07 19:07:24 -0500

    Change Picovoice access key in wakeword.ts
    Updated the Picovoice access key for wake word detection.

--- src/utils/wakeword.ts
+++ src/utils/wakeword.ts
@@ -2,7 +2,7 @@ import { PorcupineWorker } from "@picovoice/porcupine-web";
 
 export async function initWakeWord(onWake: () => void) {
   const porcupine = await PorcupineWorker.create(
-    "YOUR_PICOVOICE_ACCESS_KEY",
+    "SOTvvgNXjr5Wm1+SipC6CYrTVWKEord97jrtGjIFU4ZUO1GpJI8Lvg==",
     [{ builtin: "Hey Ava" }],
     () => {
       console.log("Wake word detected!");
